a89a7f437eb1130e16e3f4a2ff8f4b0010062f54
Mehmet Ali YILMAZ <mehmetaliyilmaz5371@gmail.com>
2022-06-03 17:47:45 +0300

    React-Native-Project
    React Native

--- App.js
+++ App.js
@@ -1,15 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image ,StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker'
 
+//HTML kullanımı gibi farkı yok etiketler ve style'lar aşşağıdan alınarak yapılır
 export default function App() {
+  
+  //Galeri erişim izni alma ve galeriden fotoğraf seçme bloğu
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Image source = {logo} style = {styles.logo}/>
+      
+      <Text style = {styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.buttonStyle}>
+          <Text style = {styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+//CSS STYLE buradan tanımlanıp yukarıda kullanılır
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +49,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom:50,
+  },
+  instructions:{
+    color:'#888',
+    fontSize: 18,
+    marginHorizontal: 50,
+  },
+  buttonStyle: {
+    backgroundColor: 'gray',
+    fontSize: 20,
+    margin: 10,
+    borderRadius: 10,
+    padding: 5,
+  },
+  buttonText:{
+    fontSize: 20,
+    color: '#fff',
+  },
+
 });
